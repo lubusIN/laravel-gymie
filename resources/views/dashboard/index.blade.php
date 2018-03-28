853a2d197a7be0697cf533dafc8b40e50d22a79a
imanghafoori <imanghafoori1@gmail.com>
2018-03-28 16:20:18 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -507,28 +507,7 @@
                                 </div>
 
                                 <div class="tab-pane fade" id="deposited">
-                                    <div class="table-responsive <?php echo(! $depositedCheques->isEmpty() ? "panel-scroll" : "") ?>">
-                                        <table class="table table-hover">
-                                            @forelse($depositedCheques as $depositedCheque)
-                                                <tr>
-                                                    <td>{{ $depositedCheque->number }}</td>
-                                                    <td>{{ $depositedCheque->date }}</td>
-                                                    <td>{{ $depositedCheque->payment->payment_amount }}</td>
-                                                    <td>
-                                                        <a href="{{ action('PaymentsController@chequeBounce',['id' => $depositedCheque->payment_id]) }}"
-                                                           class="btn btn-xs btn-danger pull-right margin-left-5">Mark Bounced</a>
-                                                        <a class="btn btn-xs btn-success pull-right"
-                                                           href="{{ action('PaymentsController@clearCheque',['id' => $depositedCheque->payment_id]) }}">Mark
-                                                            Cleared</a>
-                                                    </td>
-                                                </tr>
-                                            @empty
-                                                <div class="tab-empty-panel font-size-24 color-grey-300">
-                                                    No Data
-                                                </div>
-                                            @endforelse
-                                        </table>
-                                    </div>
+                                    @include('dashboard._index.deposited', ['depositedCheques' =>  $depositedCheques])
                                 </div>
 
                                 <div class="tab-pane fade" id="bounced">
