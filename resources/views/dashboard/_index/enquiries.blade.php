a69b5611f41fc265c1a18fd20ac9cb9033981cc2
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 12:26:08 +0430

    extract a partial

---
+++ resources/views/dashboard/_index/enquiries.blade.php
@@ -0,0 +1,22 @@
+<div class="table-responsive <?php echo(! $enquiries->isEmpty() ? "panel-scroll" : "") ?>">
+    <table class="table table-hover table-condensed">
+        @forelse($enquiries as $enquiry)
+            <tr>
+                <td><i class="fa fa-user color-blue-grey-100 fa-lg"></i></td>
+                <td><a href="{{ action('EnquiriesController@show',['id' => $enquiry->id]) }}">{{ $enquiry->name }}</a></td>
+                <td><a href="{{ action('EnquiriesController@show',['id' => $enquiry->id]) }}">{{ $enquiry->email }}</a></td>
+                <td><a href="{{ action('EnquiriesController@show',['id' => $enquiry->id]) }}">{{ $enquiry->contact }}</a>
+                </td>
+            </tr>
+        @empty
+            <div class="tab-empty-panel font-size-24 color-grey-300">
+                No Data
+            </div>
+        @endforelse
+    </table>
+</div>
+
+@if(!$enquiries->isEmpty())
+    <a class="btn btn-color btn-xs palette-concrete pull-right margin-right-10 margin-top-10"
+       href="{{ action('EnquiriesController@index') }}">View All</a>
+@endif
