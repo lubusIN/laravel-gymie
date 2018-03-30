155cce5db021f76cea2fe16a769ba7a0a6b1be41
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 14:29:37 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -148,25 +148,7 @@
                             <!-- Tab Content -->
                             <div class="tab-content">
                                 <div class="tab-pane fade in active" id="due">
-                                    <div class="table-responsive <?php echo(! $dues->isEmpty() ? "panel-scroll" : "") ?>">
-                                        <table class="table table-hover table-condensed">
-                                            @forelse($dues as $due)
-                                                <tr>
-                                                    <td><a href="{{ action('ExpensesController@edit',['id' => $due->id]) }}">{{ $due->name }}</a></td>
-                                                    <td><a href="{{ action('ExpensesController@edit',['id' => $due->id]) }}">{{ $due->amount }}</a></td>
-                                                    <td>
-                                                        <a href="{{ action('ExpensesController@edit',['id' => $due->id]) }}">{{ $due->due_date->format('Y-m-d') }}</a>
-                                                    </td>
-                                                    <td><a class="btn btn-info btn-xs btn pull-right"
-                                                           href="{{ action('ExpensesController@paid',['id' => $due->id]) }}">Pay</a></td>
-                                                </tr>
-                                            @empty
-                                                <div class="tab-empty-panel font-size-24 color-grey-300">
-                                                    No Data
-                                                </div>
-                                            @endforelse
-                                        </table>
-                                    </div>
+                                    @include('dashboard._index.due', ['dues' => $dues])
                                 </div>
 
                                 <div class="tab-pane fade" id="outstanding">
