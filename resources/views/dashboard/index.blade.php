b176c450ef51c5059cea8f01a4edce1381e4b180
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 12:51:52 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -43,26 +43,7 @@
 
                 <!-- Registrations This Weeks -->
                 <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="panel bg-teal-400">
-                        <div class="panel-body padding-15-20">
-                            <div class="clearfix">
-                                <div class="pull-left">
-                                    <div class="color-white font-size-24 font-roboto font-weight-600" data-toggle="counter" data-start="0" data-from="0"
-                                         data-to="{{ App\Member::whereMonth('created_at','=',Carbon::today()->month)->count() }}" data-speed="500"
-                                         data-refresh-interval="10"></div>
-                                </div>
-                                <div class="pull-right">
-                                    <i class="font-size-24 color-teal-100 fa fa-signal"></i>
-                                </div>
-
-                                <div class="clearfix"></div>
-
-                                <div class="pull-left">
-                                    <div class="display-block color-teal-50 font-weight-600">Monthly Joinings</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div><!-- /.panel -->
+                    @include('dashboard._index.registersThisWeek')
                 </div>
 
                 <!-- Inactive Members -->
