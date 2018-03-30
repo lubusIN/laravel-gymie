b0d96569851f017ae857db9ea16bd8218a2765de
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 12:52:02 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -58,25 +58,7 @@
 
                 <!-- Outstanding Payments -->
                 <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="panel bg-red-400">
-                        <div class="panel-body padding-15-20">
-                            <div class="clearfix">
-                                <div class="pull-left">
-                                    <div class="color-white font-size-24 font-roboto font-weight-600" data-toggle="counter" data-start="0" data-from="0"
-                                         data-to="{{ App\Invoice::sum('pending_amount') }}" data-speed="500" data-refresh-interval="10"></div>
-                                </div>
-                                <div class="pull-right">
-                                    <i class="font-size-24 color-red-100 fa fa-money"></i>
-                                </div>
-
-                                <div class="clearfix"></div>
-
-                                <div class="pull-left">
-                                    <div class="display-block color-red-50 font-weight-600">Pending Payments</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div><!-- /.panel -->
+                    @include('dashboard._index.outstandingPayments')
                 </div>
 
                 <!-- Collection -->
