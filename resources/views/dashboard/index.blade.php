1f29560d907d3cf15b0fe1dfb818b829c7912abe
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 14:40:12 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -188,24 +188,7 @@
                                 </div>
 
                                 <div class="tab-pane fade" id="bounced">
-                                    <div class="table-responsive <?php echo(! $bouncedCheques->isEmpty() ? "panel-scroll" : "") ?>">
-                                        <table class="table table-hover">
-                                            @forelse($bouncedCheques as $bouncedCheque)
-                                                <tr>
-                                                    <td>{{ $bouncedCheque->number }}</td>
-                                                    <td>{{ $bouncedCheque->date }}</td>
-                                                    <td>{{ $bouncedCheque->payment->payment_amount }}</td>
-                                                    <td><a class="btn btn-info btn-xs btn pull-right"
-                                                           href="{{ action('PaymentsController@chequeReissue',['id' => $bouncedCheque->payment_id]) }}">Reissued</a>
-                                                    </td>
-                                                </tr>
-                                            @empty
-                                                <div class="tab-empty-panel font-size-24 color-grey-300">
-                                                    No Data
-                                                </div>
-                                            @endforelse
-                                        </table>
-                                    </div>
+                                    @include('dashboard._index.deposited', ['bouncedCheques' =>  $bouncedCheques])
                                 </div>
 
                             </div>
