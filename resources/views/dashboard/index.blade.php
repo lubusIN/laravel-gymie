58799dc98028aad80ea6c78c8be394ab4be13dbd
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 14:39:35 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -255,24 +255,7 @@
                                     </div>
                                 @endif
                             </div>
-                            <div class="table-responsive <?php echo(! $smslogs->isEmpty() ? "panel-scroll-2" : "") ?>">
-                                <table class="table table-hover">
-                                    @forelse($smslogs as $smslog)
-                                        <tr>
-                                            <td>{{ $smslog->number }}</td>
-                                            <td>{{ $smslog->status }}</td>
-                                        </tr>
-                                    @empty
-                                        <div class="tab-empty-panel sms-empty-panel font-size-24 color-grey-300">
-                                            No Data
-                                        </div>
-                                    @endforelse
-                                </table>
-                            </div>
-                            @if(!$smslogs->isEmpty())
-                                <a class="btn btn-color btn-xs palette-concrete pull-right margin-right-10 margin-top-10"
-                                   href="{{ action('SmsController@logIndex') }}">View All</a>
-                            @endif
+                            @include('dashboard._index.smsLog', ['smslogs' => $smslogs])
                         </div>
                     </div>
                 </div>
