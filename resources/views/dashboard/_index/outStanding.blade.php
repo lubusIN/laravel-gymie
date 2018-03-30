b6d6cdd8a6a0372f856a65a0b53c1ec1d3cbf8c3
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 14:29:53 +0430

    extract a partial

---
+++ resources/views/dashboard/_index/outStanding.blade.php
@@ -0,0 +1,25 @@
+
+<div class="table-responsive <?php echo(! $outstandings->isEmpty() ? "panel-scroll" : "") ?>">
+    <table class="table table-hover">
+        @forelse($outstandings as $outstanding)
+            <tr>
+                <td>
+                    <a href="{{ action('ExpensesController@edit',['id' => $outstanding->id]) }}">{{ $outstanding->name }}</a>
+                </td>
+                <td>
+                    <a href="{{ action('ExpensesController@edit',['id' => $outstanding->id]) }}">{{ $outstanding->amount }}</a>
+                </td>
+                <td>
+                    <a href="{{ action('ExpensesController@edit',['id' => $outstanding->id]) }}">{{ $outstanding->due_date->format('Y-m-d') }}</a>
+                </td>
+                <td><a class="btn btn-info btn-xs btn pull-right"
+                       href="{{ action('ExpensesController@paid',['id' => $outstanding->id]) }}">Pay</a></td>
+            </tr>
+        @empty
+            <div class="tab-empty-panel font-size-24 color-grey-300">
+                No Data
+            </div>
+        @endforelse
+    </table>
+</div>
+                                
