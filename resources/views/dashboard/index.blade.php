f874f70efb7671f565dbd20c1998d7f3c0bee88a
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 12:28:28 +0430

    extract a partial

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -231,30 +231,7 @@
                                 </div>
 
                                 <div class="tab-pane fade" id="reminders">
-                                    <div class="table-responsive <?php echo(! $reminders->isEmpty() ? "panel-scroll" : "") ?>">
-                                        <table class="table table-hover">
-                                            @forelse($reminders as $reminder)
-                                                <tr>
-                                                    <td>
-                                                        <a href="{{ action('EnquiriesController@show',['id' => $reminder->enquiry->id]) }}">{{ $reminder->enquiry->name }}</a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="{{ action('EnquiriesController@show',['id' => $reminder->enquiry->id]) }}">{{ $reminder->enquiry->contact }}</a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="{{ action('EnquiriesController@show',['id' => $reminder->enquiry->id]) }}">{{ $reminder->due_date->format('Y-m-d') }}</a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="{{ action('EnquiriesController@show',['id' => $reminder->enquiry->id]) }}">{{ Utilities::getFollowupBy ($reminder->followup_by) }}</a>
-                                                    </td>
-                                                </tr>
-                                            @empty
-                                                <div class="tab-empty-panel font-size-24 color-grey-300">
-                                                    No Data
-                                                </div>
-                                            @endforelse
-                                        </table>
-                                    </div>
+                                    @include('dashboard._index.reminders')
                                 </div>
                             </div>
                         </div>
