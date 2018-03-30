420ded66073d7cce2112bd340e8062266cbb72b9
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 13:04:50 +0430

    extract a partial

---
+++ resources/views/dashboard/_index/totalMembers.blade.php
@@ -0,0 +1,23 @@
+@php
+$count = App\Member::where('status',1)->count();
+@endphp
+<div class="panel bg-light-blue-400">
+    <div class="panel-body padding-15-20">
+        <div class="clearfix">
+            <div class="pull-left">
+                <div class="color-white font-size-24 font-roboto font-weight-600" data-toggle="counter" data-start="0" data-from="0"
+                     data-to="{{ $count }}" data-speed="500" data-refresh-interval="10"></div>
+            </div>
+
+            <div class="pull-right">
+                <i class="font-size-24 color-light-blue-100 fa fa-users"></i>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="pull-left">
+                <div class="display-block color-light-blue-50 font-weight-600">Total Members</div>
+            </div>
+        </div>
+    </div>
+</div><!-- /.panel -->
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,26 +19,7 @@
             <div class="row margin-top-10">
                 <!-- Total Members -->
                 <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="panel bg-light-blue-400">
-                        <div class="panel-body padding-15-20">
-                            <div class="clearfix">
-                                <div class="pull-left">
-                                    <div class="color-white font-size-24 font-roboto font-weight-600" data-toggle="counter" data-start="0" data-from="0"
-                                         data-to="{{ App\Member::where('status',1)->count() }}" data-speed="500" data-refresh-interval="10"></div>
-                                </div>
-
-                                <div class="pull-right">
-                                    <i class="font-size-24 color-light-blue-100 fa fa-users"></i>
-                                </div>
-
-                                <div class="clearfix"></div>
-
-                                <div class="pull-left">
-                                    <div class="display-block color-light-blue-50 font-weight-600">Total Members</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div><!-- /.panel -->
+                    @include('dashboard._index.totalMembers')
                 </div>
 
                 <!-- Registrations This Weeks -->
