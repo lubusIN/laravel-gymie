999a8165cd21c953b2a710184617059838adbe24
daspayalll <subhrahemanta@gmail.com>
2025-05-28 11:26:50 +0530

    fix: correct option label from 'Others' to 'Other' in gender selection

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -67,7 +67,7 @@ class UserResource extends Resource
                                             ->options([
                                                 'male' => 'Male',
                                                 'female' => 'Female',
-                                                'others' => 'Others'
+                                                'other' => 'Other'
                                             ])
                                             ->required()
                                             ->default('male')
