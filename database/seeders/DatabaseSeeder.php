5b76de872f4f2f1911495271344a5e535ede9747
daspayalll <subhrahemanta@gmail.com>
2025-06-05 11:58:49 +0530

    fix: add service seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SeedAction::class,
             EnquirySeeder::class,
+            ServiceSeeder::class,
             UserSeeder::class,
         ]);
     }
