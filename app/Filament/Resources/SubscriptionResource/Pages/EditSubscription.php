605d02ef1b86faec21db646e6e08dcb883d9e617
punitverma123 <punitv342@gmail.com>
2025-06-12 16:13:38 +0530

    feat: Add Subscription resource

---
+++ app/Filament/Resources/SubscriptionResource/Pages/EditSubscription.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\SubscriptionResource\Pages;
+
+use App\Filament\Resources\SubscriptionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSubscription extends EditRecord
+{
+    protected static string $resource = SubscriptionResource::class;
+
+    public function getTitle(): string
+    {
+        return 'Edit ' . ' Subscription: ' . $this->record->member->name;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
