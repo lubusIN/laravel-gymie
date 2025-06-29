8ae0b5ffbce88a581d44e2616c94ec852a63f34b
punitverma123 <punitv342@gmail.com>
2025-06-29 18:16:02 +0530

    feat(plans-table): add filter by statuses tabs

--- app/Filament/Resources/PlanResource/Pages/ListPlans.php
+++ app/Filament/Resources/PlanResource/Pages/ListPlans.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\PlanResource\Pages;
 
+use App\Enums\Status;
 use App\Filament\Resources\PlanResource;
 use App\Models\Plan;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPlans extends ListRecords
 {
@@ -16,7 +19,34 @@ class ListPlans extends ListRecords
         return [
             Actions\CreateAction::make()
                 ->icon('heroicon-m-plus')
+                ->modalAlignment('center')
+                ->modalWidth('xl')
+                ->modalHeading('New plan')
+                ->createAnother(false)
                 ->visible(Plan::exists()),
+        ];
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return [
+            'Memberships',
+            'Plans',
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'active' => Tab::make('Active')
+                ->badge(Plan::query()->where('status', 'active')->count())
+                ->badgeColor(Status::Active->getColor())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'active')),
+            'inactive' => Tab::make('Inactive')
+                ->badge(Plan::query()->where('status', 'inactive')->count())
+                ->badgeColor(Status::Inactive->getColor())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'inactive')),
         ];
     }
 }
